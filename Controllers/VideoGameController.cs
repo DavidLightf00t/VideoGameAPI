a5940970f68b809d293ef625fccc94f543958ff9
David Lightfoot <dslightfoot@yahoo.com>
2025-05-23 21:33:18 -0400

    Update CRUD operations to use database

--- Controllers/VideoGameController.cs
+++ Controllers/VideoGameController.cs
@@ -1,103 +1,92 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using VideoGameApi.Data;
 
 namespace VideoGameAPI.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
 
-    public class VideoGameController : ControllerBase
+    public class VideoGameController(VideoGameDbContext context) : ControllerBase
     {
-        static private List<VideoGame> videoGames = new List<VideoGame>
-        {
-            new VideoGame
-            {
-                Id = 1,
-                Title = "Spider-Man 2",
-                Platform = "PS5",
-                Developer = "Insomniac Games",
-                Publisher = "Sony Interactive Entertainment"
-            },
-            new VideoGame
-            {
-                Id = 2,
-                Title = "The Legend of Zelda: Breath of the Wild",
-                Platform = "Nintendo Switch",
-                Developer = "Nintendo EPD",
-                Publisher = "Nintendo"
-            },
-            new VideoGame
-            {
-                Id = 3,
-                Title = "Cyberpunk 2077",
-                Platform = "PC",
-                Developer = "CD Projekt Red",
-                Publisher = "CD Projekt"
-            }
-        };
-        
-        // Get entire list of video games
+        // Use dependency injection to inject the database
+        // VideoGameDbContext constructor
+        private readonly VideoGameDbContext _context = context;
+
         [HttpGet]
-        public ActionResult<List<VideoGame>> GetVideoGames()
+        public async Task<ActionResult<List<VideoGame>>> GetVideoGames()
         {
-            return Ok(videoGames);
+            // Use _context object to get the VideoGames table
+            return Ok(await _context.VideoGames.ToListAsync());
         }
 
         // Get single video game with Id parameter
         [HttpGet("{id}")]
-        public ActionResult<VideoGame> GetVideoGameById(int id)
+        public async  Task<ActionResult<VideoGame>> GetVideoGameById(int id)
         {
-            var game = videoGames.FirstOrDefault(game => game.Id == id);
+            var game = await _context.VideoGames.FindAsync(id);
             if (game is null)
             {
                 return NotFound();
             }
-
+        
             return Ok(game);
         }
-
+        
         // Add a new video game
         [HttpPost]
-        public ActionResult<VideoGame> AddVideoGame(VideoGame newGame)
+        public async Task<ActionResult<VideoGame>> AddVideoGame(VideoGame newGame)
         {
             if (newGame is null)
             {
                 return BadRequest();
             }
-
-            newGame.Id = videoGames.Max(game => game.Id) + 1;
-            videoGames.Add(newGame);
+        
+            // Make changes to the given table
+            _context.VideoGames.Add(newGame);
+            // Store and save changes to database
+            await _context.SaveChangesAsync();
+            
             return CreatedAtAction(nameof(GetVideoGameById), new { id = newGame.Id }, newGame);
         }
-
+        
         // Update a video game
         [HttpPut("{id}")]
-        public IActionResult UpdateVideoGame(int id, VideoGame updatedGame)
+        public async Task<IActionResult> UpdateVideoGame(int id, VideoGame updatedGame)
         {
-            var game = videoGames.FirstOrDefault(game => game.Id == id);
+            // Use _context object to get VideoGames table then find the game id from that table
+            var game = await _context.VideoGames.FindAsync(id);
             if (game is null)
             {
                 return NotFound();
             }
-
+        
             game.Title = updatedGame.Title;
             game.Platform = updatedGame.Platform;
             game.Developer = updatedGame.Developer;
             game.Publisher = updatedGame.Publisher;
-
+            
+            // Save and update changes
+            await _context.SaveChangesAsync();
+        
             return NoContent();
         }
         
         // Delete a video game
         [HttpDelete("{id}")]
-        public IActionResult DeleteVideoGame(int id)
+        public async Task<IActionResult> DeleteVideoGame(int id)
         {
-            var game = videoGames.FirstOrDefault(game => game.Id == id);
+            // Use _context object to get VideoGames table and find the VideoGame with the specified id
+            var game = await _context.VideoGames.FindAsync(id);
             if (game is null)
             {
                 return NotFound();
             }
-
-            videoGames.Remove(game);
+        
+            // Remove the video game that was specified above
+            _context.VideoGames.Remove(game);
+            // Save and apply changed to the database
+            await _context.SaveChangesAsync();
             return NoContent();
         }
     }
